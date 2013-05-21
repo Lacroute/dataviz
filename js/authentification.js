a060e7ce6b82545282e29646af4ff1e85820e920
Procureur <mc.flurry@hotmail.fr>
2013-05-21 12:15:14 +0200

    authirize -> authentificate

--- js/authentification.js
+++ js/authentification.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
 	var foursquareConnector = {
 		clientId: '0PCSDR4BPTWOSAHNTQQX2Z4C2JCBLZGA3BO5GC5N0E2510BV',
 		clientSecret: 'IOWDTFOLWYJZ0GAULQNDYGNVPFIUMN11KV4CZGRKE1GYVI1U',
-		responseType: 'zboub',
 		redirectUrl: 'http://localhost/dataviz/ok.html',
-		authorize: function(){
+		authenticate: function(){
 			var url = "https://foursquare.com/oauth2/authenticate";
 			url += "?client_id="+this.clientId;
 			url += '&response_type=token';
@@ -15,6 +14,6 @@ $(document).ready(function(){
 	};
 	$('#foursquareConnect').click(function(event){
 		event.preventDefault();
-		foursquareConnector.authorize();
+		foursquareConnector.authenticate();
 	});
 });
