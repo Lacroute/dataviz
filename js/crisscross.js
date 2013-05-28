96ad63e34970ce33295274e648c62556825818af
Thomas Senkman <thomas@senkman.fr>
2013-05-28 19:08:27 +0200

    getHour ok

--- js/crisscross.js
+++ js/crisscross.js
@@ -2,35 +2,80 @@ var user = {};
 
 $(document).ready(function(){
 
-user.init = function(data){
+	user.init = function(data){
+		
+		user.id = data.response.user.id;
+		user.lastName = data.response.user.lastName;
+		user.firstName = data.response.user.firstName;
+		user.city = data.response.user.homeCity;
+		user.mayorshipNumber = data.response.user.mayorships.count;
+		user.tipsNumber = data.response.user.tips.count;
+		user.followNumber = data.response.user.following.count;
+		user.checkinsNumber = data.response.user.checkins.count;
+		
+		//checkins
+		jQuery.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: "https://api.foursquare.com/v2/users/self/checkins?limit=200&oauth_token="+getUrlParam('access_token'),
+			success: function(data, textStatus, jqXHR) {
+				console.log(textStatus);
+				user.checkins = data.response.checkins.items;
+				},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log('KO');
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+		});
+		console.log(user);
+		
+	}
 	
-	user.id = data.response.user.id;
-	user.lastName = data.response.user.lastName;
-	user.firstName = data.response.user.firstName;
-	user.city = data.response.user.homeCity;
-	user.mayorshipNumber = data.response.user.mayorships.count;
-	user.tipsNumber = data.response.user.tips.count;
-	user.followNumber = data.response.user.following.count;
-	user.checkinsNumber = data.response.user.checkins.count;
-	
-	//checkins
-	jQuery.ajax({
-		type: 'GET',
-		dataType: 'json',
-		url: "https://api.foursquare.com/v2/users/self/checkins?limit=200&oauth_token="+getUrlParam('access_token'),
-		success: function(data, textStatus, jqXHR) {
-			console.log(textStatus);
-			user.checkins = data.response.checkins;
-			},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log('KO');
-			console.log(textStatus);
-			console.log(errorThrown);
+	user.getHour = function(){
+		var coucheTard = 0,
+			insomniaque = 0,
+			matinal = 0,
+			diurne = 0;
+			
+		$.each(user.checkins, function (index, value) {
+			var hr = value.createdAt%86400;
+			var heure = 3600;
+			
+			if(hr<2*heure){
+				coucheTard++;
+				console.log("coucheTard");
+			}else if(hr<6*heure){
+				insomniaque++;
+				console.log("insomniaque");
+			}else if(hr<10*heure){
+				matinal = matinal + 1;
+				console.log("matinal");
+			}else if(hr<22*heure){
+				diurne++;
+				console.log("diurne");
+			}else{
+				coucheTard++;
+				console.log("coucheTard");
+			}
+		})
+		
+		var max = Math.max(coucheTard, insomniaque ,matinal ,diurne);
+		console.log("tard :"+coucheTard);
+		console.log("atin :"+matinal);
+		console.log("diurne :"+diurne);
+		console.log("insomniaque:"+insomniaque);
+		
+		if(max==coucheTard){
+			console.log("Resultat : Couche tard !");
+		}else if(max==matinal){
+			console.log("Resultat : Matinal !");
+		}else if(max==diurne){
+			console.log("Resultat : Diurne !");	
+		}else{
+			console.log("Resultat : Insomniaque !");	
 		}
-	});
-	console.log(user);
-	
-}	
+	}
 
 
 	// Fonction de Clément pour récuperer le Token dans l'url
