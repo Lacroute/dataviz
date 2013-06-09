7ded73501deaf026d89520815ea7d5e0fbe8dd82
guillaumemarq <contact@guillaume-marq.com>
2013-06-09 14:34:02 +0200

    box note global structure

--- inte/js/main.js
+++ inte/js/main.js
@@ -3,11 +3,19 @@ $(document).ready(function(){
 $('#openMenu').on('click', function(){
 
 	$('#content').toggleClass('on');
+});
 
+$('#openNote').on('click', function(){
+
+	$('#note').toggleClass('on');
+});
+
+$('#closeNote').on('click', function(){
+
+	$('#note').toggleClass('on');
 });
 
 $('#menu').on('click', function(){
-
 	$('#content').toggleClass('on');
 
 
