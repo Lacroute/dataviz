7ea854dd8eaa8bba4807d5a7653ffbb79a5b31fe
Thomas Senkman <thomas@senkman.fr>
2013-06-10 11:11:35 +0200

    Fix menu (Clement)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
 $(document).ready(function(){
 
-$('#openMenu').on('click', function(event){
-	event.preventDefault();
-	$('#content').toggleClass('on');
-});
+    $('#content header').on('click', function(event){
+            event.preventDefault();
+            $('#content').toggleClass('on');
+            $('#svgContent').toggleClass('on');
+    });
 
-$('#openNote').on('click', function(event){
-	event.preventDefault();
-	$('#note').toggleClass('on');
-});
+    $('#openNote').on('click', function(event){
+    	event.preventDefault();
+    	$('#note').toggleClass('on');
+    });
 
-$('#closeNote').on('click', function(event){
-	event.preventDefault();
-	$('#note').toggleClass('on');
-});
+    $('#closeNote').on('click', function(event){
+    	event.preventDefault();
+    	$('#note').toggleClass('on');
+    });
 
-$('#menu').on('click', function(event){
-	event.preventDefault();
-	$('#content').toggleClass('on');
-});
+    $('#menu a').on('click', function(event){
+    	event.preventDefault();
+    	$('#content').toggleClass('on');
+    });
+
+    $('#svgContent').on('click', function(event){
+        event.preventDefault();
+        if($(this).is('.on')){
+            $('#content').toggleClass('on');
+            $('#svgContent').toggleClass('on');
+        }
+    });
+
 	
 });
 
