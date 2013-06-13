aa493fd1a0bb040143c4f4eb9c46e522c84e660b
Procureur <mc.flurry@hotmail.fr>
2013-06-14 00:40:17 +0200

    spantoine

--- js/listener.js
+++ js/listener.js
@@ -15,17 +15,25 @@ function setBadges () {
         updateComportement(json.badges[0].label, json.badges[0].description, $(this).offset().top);
     });
 
+    $('#check p').html(json.badges[1].nb);
+    $('#check').on('click', function(event){
+        event.preventDefault();
+        updateComportement(json.badges[1].label, json.badges[1].description, $(this).offset().top);
+    });
+
     /*** [END] BADGES ***/
 
     /*** DESCRIPTION ***/
     
-    if(json.badges[4].length != undefined){
-        comportementLabel = json.badges[4].label + ' ';
-        comportementDescription = '<p>'+json.badges[4].description+'</p>';
+    function initComportement(){
+        if(json.badges[4].length != undefined){
+            comportementLabel = json.badges[4].label + ' ';
+            comportementDescription = '<p>'+json.badges[4].description+'</p>';
+        }
+        comportementLabel += json.badges[5].label;
+        comportementDescription += '<p>'+json.badges[5].description+'</p>';
     }
-    comportementLabel += json.badges[5].label;
-    comportementDescription += '<p>'+json.badges[5].description+'</p>';
-
+    
     function updateComportement(label, description, toOffset){
         cl.html(label);
         cd.html(description);
