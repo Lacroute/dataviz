9f526cba6b955cf2da7fe20431ca9076c2f80067
Procureur <mc.flurry@hotmail.fr>
2013-06-14 00:34:45 +0200

    structure click&scroll pour badges

--- js/listener.js
+++ js/listener.js
@@ -1,5 +1,7 @@
 function setBadges () {
     console.log(json);
+    var comportementLabel = '', comportementDescription = '', cl = $('#comportementLabel'), cd = $('#comportementDescription');
+
     /*** JSON.GENERAL ***/
     $('#nomProfil').html(json.general.nom_complet);
     $('#nomDescription').html(json.general.prenom);
@@ -7,19 +9,27 @@ function setBadges () {
     /*** [END] JSON.GENERAL ***/
 
     /*** BADGES ***/
-    $('#grade img').attr('src', 'media/badge_'+json.badges[0].label+'.png')
+    $('#grade img').attr('src', 'media/badge_'+json.badges[0].label+'.png');
+    $('#grade').on('click', function(event){
+        event.preventDefault();
+        updateComportement(json.badges[0].label, json.badges[0].description, $(this).offset().top);
+    });
 
     /*** [END] BADGES ***/
 
     /*** DESCRIPTION ***/
-    var comportementLabel = '', comportementDescription = '';
+    
     if(json.badges[4].length != undefined){
         comportementLabel = json.badges[4].label + ' ';
         comportementDescription = '<p>'+json.badges[4].description+'</p>';
     }
     comportementLabel += json.badges[5].label;
     comportementDescription += '<p>'+json.badges[5].description+'</p>';
-    $('#comportementLabel').html(comportementLabel);
-    $('#comportementDescription').html(comportementDescription);
+
+    function updateComportement(label, description, toOffset){
+        cl.html(label);
+        cd.html(description);
+        if(toOffset>0) $('#content').animate({scrollTop: toOffset});
+    }
     /*** [END] DESCRIPTION ***/
 }
