a44dde64793af6d0cfc74cc4042deaec47cb0e9a
Procureur <mc.flurry@hotmail.fr>
2013-06-14 00:05:37 +0200

    injection label + description

--- js/listener.js
+++ js/listener.js
@@ -1,5 +1,18 @@
 function setBadges () {
-    console.log(json);
+    console.log(json.badges[5]);
     $('.nomProfil').html(json.general.nom);
     $('#avatar').attr('src', json.general.photo);
+    var comportementLabel = '', comportementDescription = '';
+    console.log(json.badges[4]);
+    if(json.badges[4].length != undefined){
+        console.log('caca');
+        comportementLabel = json.badges[4].label + ' ';
+        comportementDescription = '<p>'+json.badges[4].description+'</p>';
+        console.log('********* '+comportementLabel);
+    }
+    comportementLabel += json.badges[5].label;
+    comportementDescription += '<p>'+json.badges[5].description+'</p>';
+    $('#comportementLabel').html(comportementLabel);
+    $('#comportementDescription').html(comportementDescription);
+
 }
